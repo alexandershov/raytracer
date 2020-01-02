6bbdd7485728ddd9a130e5abe1703e1fbfe31408
Alexander Ershov <aershov182@yandex-team.ru>
2020-01-02 11:53:57 +0300

    black

--- src/raytracer/algebra.py
+++ src/raytracer/algebra.py
@@ -5,10 +5,10 @@ from typing import List
 
 def solve_quadratic(a, b, c) -> List[float]:
     if a == 0 and b == 0:
-        raise ValueError(f'not a quadratic equation: {a}x^2 + {b}x + c')
+        raise ValueError(f"not a quadratic equation: {a}x^2 + {b}x + c")
     if a == 0:
         return [-c / b]
-    d = (b ** 2 - 4 * a * c)
+    d = b ** 2 - 4 * a * c
     if d < 0:
         return []
     return list({(-b - math.sqrt(d)) / (2 * a), (-b + math.sqrt(d)) / (2 * a)})
--- src/tests/test_algebra.py
+++ src/tests/test_algebra.py
@@ -3,13 +3,10 @@ import pytest
 from raytracer import algebra
 
 
-@pytest.mark.parametrize("a, b, c, expected", [
-    (1, 10, 25, [-5]),
-    (1, 0, -4, [-2, 2]),
-    (1, 0, 4, []),
-    (0, 2, 4, [-2]),
-
-])
+@pytest.mark.parametrize(
+    "a, b, c, expected",
+    [(1, 10, 25, [-5]), (1, 0, -4, [-2, 2]), (1, 0, 4, []), (0, 2, 4, [-2])],
+)
 def test_solve_quadratic(a, b, c, expected):
     assert sorted(algebra.solve_quadratic(a, b, c)) == list(
         map(pytest.approx, expected)
