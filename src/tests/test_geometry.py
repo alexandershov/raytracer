e5a4aea95d897d34559dbfe94424d75d68d9079b
Alexander Ershov <aershov182@yandex-team.ru>
2020-01-02 09:29:45 +0300

    extract test constants

--- src/tests/test_geometry.py
+++ src/tests/test_geometry.py
@@ -1,23 +1,21 @@
 from raytracer import geometry
 
+START = geometry.Point(3, 2, 1)
+AFTER_START = geometry.Point(9, 6, 3)
+DIRECTION = geometry.Vector(6, 4, 2)
+
 
 def test_sub_points():
-    a = geometry.Point(3, 2, 1)
-    b = geometry.Point(9, 6, 3)
-    assert (b - a) == geometry.Vector(6, 4, 2)
+    assert (AFTER_START - START) == DIRECTION
 
 
 def test_create_ray_from_start_and_direction():
-    start = geometry.Point(3, 2, 1)
-    direction = geometry.Vector(6, 4, 2)
-    ray = geometry.Ray(start, direction)
-    assert ray.start == start
-    assert ray.direction == direction
+    ray = geometry.Ray(START, DIRECTION)
+    assert ray.start == START
+    assert ray.direction == DIRECTION
 
 
 def test_create_ray_from_points():
-    start = geometry.Point(3, 2, 1)
-    after_start = geometry.Point(9, 6, 3)
-    ray = geometry.Ray.from_points(start, after_start)
-    assert ray.start == start
-    assert ray.direction == geometry.Vector(6, 4, 2)
+    ray = geometry.Ray.from_points(START, AFTER_START)
+    assert ray.start == START
+    assert ray.direction == DIRECTION
