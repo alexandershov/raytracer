9b725ca83960766c56b85178d735f277a7a28710
Alexander Ershov <aershov182@yandex-team.ru>
2020-01-02 09:22:23 +0300

    create Ray from two points

--- src/raytracer/geometry.py
+++ src/raytracer/geometry.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import dataclasses
 
 
@@ -6,6 +8,11 @@ class Point:
     x: float
     y: float
     z: float
+
+    def __sub__(self, other) -> Vector:
+        if not isinstance(other, Point):
+            return NotImplemented
+        return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
 
 
 @dataclasses.dataclass(frozen=True)
@@ -19,3 +26,8 @@ class Vector:
 class Ray:
     start: Point
     direction: Vector
+
+    @staticmethod
+    def from_points(start: Point, after_start: Point) -> Ray:
+        assert start != after_start
+        return Ray(start, after_start - start)
--- src/tests/test_geometry.py
+++ src/tests/test_geometry.py
@@ -2,8 +2,8 @@ from raytracer import geometry
 
 
 def test_ray():
-    start = geometry.Point(0, 0, 0)
-    direction = geometry.Vector(1, 1, 1)
-    ray = geometry.Ray(start, direction)
+    start = geometry.Point(3, 2, 1)
+    after_start = geometry.Point(9, 6, 3)
+    ray = geometry.Ray.from_points(start, after_start)
     assert ray.start == start
-    assert ray.direction == direction
+    assert ray.direction == geometry.Vector(6, 4, 2)
