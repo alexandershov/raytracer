78f772be8c40181d32e5e4fef2473061e2dc3ed2
Alexander Ershov <aershov182@yandex-team.ru>
2020-01-02 11:51:46 +0300

    handle negative discriminant

--- src/raytracer/algebra.py
+++ src/raytracer/algebra.py
@@ -4,5 +4,7 @@ from typing import List
 
 
 def solve_quadratic(a, b, c) -> List[float]:
-    d = math.sqrt(b ** 2 - 4 * a * c)
-    return list({(-b - d) / (2 * a), (-b + d) / (2 * a)})
+    d = (b ** 2 - 4 * a * c)
+    if d < 0:
+        return []
+    return list({(-b - math.sqrt(d)) / (2 * a), (-b + math.sqrt(d)) / (2 * a)})
--- src/tests/test_algebra.py
+++ src/tests/test_algebra.py
@@ -3,7 +3,12 @@ import pytest
 from raytracer import algebra
 
 
-@pytest.mark.parametrize("a, b, c, expected", [(1, 10, 25, [-5]), (1, 0, -4, [-2, 2])])
+@pytest.mark.parametrize("a, b, c, expected", [
+    (1, 10, 25, [-5]),
+    (1, 0, -4, [-2, 2]),
+    (1, 0, 4, []),
+
+])
 def test_solve_quadratic(a, b, c, expected):
     assert sorted(algebra.solve_quadratic(a, b, c)) == list(
         map(pytest.approx, expected)
