cf3b4fc30988b95839ec8a8b5775e28766ac312d
Alexander Ershov <aershov182@yandex-team.ru>
2020-01-02 11:48:45 +0300

    test 2 solutions in quadratic equation

--- src/tests/test_algebra.py
+++ src/tests/test_algebra.py
@@ -3,9 +3,8 @@ import pytest
 from raytracer import algebra
 
 
-@pytest.mark.parametrize('a, b, c, expected', [
-    (1, 10, 25, [-5]),
-    # (1, 0, -4, [-2, 2]),
-])
+@pytest.mark.parametrize("a, b, c, expected", [(1, 10, 25, [-5]), (1, 0, -4, [-2, 2])])
 def test_solve_quadratic(a, b, c, expected):
-    assert sorted(algebra.solve_quadratic(a, b, c)) == list(map(pytest.approx, expected))
+    assert sorted(algebra.solve_quadratic(a, b, c)) == list(
+        map(pytest.approx, expected)
+    )
