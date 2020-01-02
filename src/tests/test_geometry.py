309774156877dfa67fd302d8aa2a948ffc48658c
Alexander Ershov <aershov182@yandex-team.ru>
2020-01-02 09:27:44 +0300

    test create Ray from start and direction

--- src/tests/test_geometry.py
+++ src/tests/test_geometry.py
@@ -7,6 +7,14 @@ def test_sub_points():
     assert (b - a) == geometry.Vector(6, 4, 2)
 
 
+def test_create_ray_from_start_and_direction():
+    start = geometry.Point(3, 2, 1)
+    direction = geometry.Vector(6, 4, 2)
+    ray = geometry.Ray(start, direction)
+    assert ray.start == start
+    assert ray.direction == direction
+
+
 def test_create_ray_from_points():
     start = geometry.Point(3, 2, 1)
     after_start = geometry.Point(9, 6, 3)
