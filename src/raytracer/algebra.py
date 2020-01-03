8240a536794cc759ffa4018450f86e8bde88e892
Alexander Ershov <aershov182@yandex-team.ru>
2020-01-03 22:31:41 +0300

    add solve_quadratic without numpy

--- src/raytracer/algebra.py
+++ src/raytracer/algebra.py
@@ -1,6 +1,7 @@
 import math
-
 from typing import List
+
+import numpy as np
 
 
 def solve_quadratic(a, b, c) -> List[float]:
@@ -12,3 +13,7 @@ def solve_quadratic(a, b, c) -> List[float]:
     if d < 0:
         return []
     return list({(-b - math.sqrt(d)) / (2 * a), (-b + math.sqrt(d)) / (2 * a)})
+
+
+def solve_quadratic_numpy(a, b, c) -> List[float]:
+    return list({x for x in np.roots([a, b, c]) if np.isreal(x)})
