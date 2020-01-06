09497aa255e34cc79699ee17bda842f830224773
aershov <codumentary.com@gmail.com>
2020-01-06 17:28:05 +0300

    extract to _solve_linear

--- src/raytracer/algebra.py
+++ src/raytracer/algebra.py
@@ -6,8 +6,14 @@ def solve_quadratic(a, b, c) -> List[float]:
     if a == 0 and b == 0:
         raise ValueError(f"`{a}x^2 + {b}x + c = 0` is not a function")
     if a == 0:
-        return [-c / b]
+        return _solve_linear(b, c)
     d = b ** 2 - 4 * a * c
     if d < 0:
         return []
     return list({(-b - math.sqrt(d)) / (2 * a), (-b + math.sqrt(d)) / (2 * a)})
+
+
+def _solve_linear(a, b) -> List[float]:
+    if a == 0:
+        raise ValueError(f"`{a}x + {b} = 0` is not a function")
+    return [-b / a]
