0c7eb93af4bba69c4da99eb798aa3cb5de21d4c0
aershov <codumentary.com@gmail.com>
2020-01-07 12:50:18 +0300

    parametrize test_color_creation_failure

--- src/tests/test_color.py
+++ src/tests/test_color.py
@@ -8,6 +8,9 @@ def test_color_mul():
     assert Color(1, 2, 3) * 200 == Color(200, 255, 255)
 
 
-def test_color_creation_failure():
+@pytest.mark.parametrize('rgb', [
+    (256, 255, 255),
+])
+def test_color_creation_failure(rgb):
     with pytest.raises(ValueError):
-        Color(256, 255, 255)
+        Color(*rgb)
