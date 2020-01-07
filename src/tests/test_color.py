ed982b2f6f9ccd57ffdf55ba451c091486a0444c
aershov <codumentary.com@gmail.com>
2020-01-07 12:47:41 +0300

    test color creation failure

--- src/tests/test_color.py
+++ src/tests/test_color.py
@@ -1,6 +1,13 @@
+import pytest
+
 from raytracer.image import Color
 
 
 def test_color():
     assert Color(1, 2, 3) * 2 == Color(2, 4, 6)
     assert Color(1, 2, 3) * 200 == Color(200, 255, 255)
+
+
+def test_color_creation_failure():
+    with pytest.raises(ValueError):
+        Color(256, 255, 255)
