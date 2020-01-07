ec0e3292f27334ca67938162b20d92172bb3f33d
aershov <codumentary.com@gmail.com>
2020-01-07 12:55:12 +0300

    parametrize test_color_mul

--- src/tests/test_color.py
+++ src/tests/test_color.py
@@ -3,9 +3,12 @@ import pytest
 from raytracer.image import Color
 
 
-def test_color_mul():
-    assert Color(1, 2, 3) * 2 == Color(2, 4, 6)
-    assert Color(1, 2, 3) * 200 == Color(200, 255, 255)
+@pytest.mark.parametrize(
+    "color, mul, expected",
+    [(Color(1, 2, 3), 2, Color(2, 4, 6)), (Color(1, 2, 3), 200, Color(200, 255, 255)),],
+)
+def test_color_mul(color, mul, expected):
+    assert color * mul == expected
 
 
 @pytest.mark.parametrize("r, g, b", [(256, 255, 255), (255, -1, 255),])
