68332891590d7175746fcfa971bd7ff58a024929
aershov <codumentary.com@gmail.com>
2020-01-07 12:48:24 +0300

    rename test

--- src/tests/test_color.py
+++ src/tests/test_color.py
@@ -3,7 +3,7 @@ import pytest
 from raytracer.image import Color
 
 
-def test_color():
+def test_color_mul():
     assert Color(1, 2, 3) * 2 == Color(2, 4, 6)
     assert Color(1, 2, 3) * 200 == Color(200, 255, 255)
 
