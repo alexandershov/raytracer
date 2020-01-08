851f68d0cdf4e94823b182483c110839f1d2b6e4
aershov <codumentary.com@gmail.com>
2020-01-08 14:46:50 +0300

    add more Interval tests

--- src/tests/test_interval.py
+++ src/tests/test_interval.py
@@ -9,6 +9,9 @@ from raytracer.interval import Interval
         (Interval(min=0), 5, True),
         (Interval(min=0), 0, True),
         (Interval(min=0), -1, False),
+        (Interval(max=0), 5, False),
+        (Interval(max=0), 0, True),
+        (Interval(max=0), -1, True),
     ],
 )
 def test_contains(interval, value, expected):
