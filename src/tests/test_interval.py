4972b701b2b4b4271368069ce0028a363fbd5a35
aershov <codumentary.com@gmail.com>
2020-01-08 14:46:20 +0300

    add more Interval tests

--- src/tests/test_interval.py
+++ src/tests/test_interval.py
@@ -3,6 +3,13 @@ import pytest
 from raytracer.interval import Interval
 
 
-@pytest.mark.parametrize("interval, value, expected", [(Interval(min=0), 5, True),])
+@pytest.mark.parametrize(
+    "interval, value, expected",
+    [
+        (Interval(min=0), 5, True),
+        (Interval(min=0), 0, True),
+        (Interval(min=0), -1, False),
+    ],
+)
 def test_contains(interval, value, expected):
     assert (value in interval) is expected
