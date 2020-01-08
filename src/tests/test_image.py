c85df90a0db6684461b5590b56757a3b81e4ee64
aershov <codumentary.com@gmail.com>
2020-01-08 12:01:10 +0300

    don't allow to set_pixel outside of the image

--- src/tests/test_image.py
+++ src/tests/test_image.py
@@ -1,3 +1,5 @@
+import pytest
+
 from raytracer.color import Palette
 from raytracer.image import Image
 
@@ -6,3 +8,9 @@ def test_image():
     image = Image(300, 200)
     image.set_pixel(5, 10, Palette.BLACK)
     assert image.get_pixel(5, 10) == Palette.BLACK
+
+
+def test_set_pixel_out_of_bounds():
+    image = Image(300, 200)
+    with pytest.raises(ValueError):
+        image.set_pixel(300, 199, Palette.BLACK)
