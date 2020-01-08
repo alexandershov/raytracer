0f43243bd80caf6f4303efb9639bc23f834c2a41
aershov <codumentary.com@gmail.com>
2020-01-08 14:49:31 +0300

    dry Interval tests

--- src/tests/test_interval.py
+++ src/tests/test_interval.py
@@ -3,20 +3,25 @@ import pytest
 from raytracer.interval import Interval
 
 
+POSITIVE_OR_ZERO = Interval(min=0)
+NEGATIVE_OR_ZERO = Interval(max=0)
+ZERO_TO_ONE = Interval(min=0, max=1)
+
+
 @pytest.mark.parametrize(
     "interval, value, expected",
     [
-        (Interval(min=0), 5, True),
-        (Interval(min=0), 0, True),
-        (Interval(min=0), -1, False),
-        (Interval(max=0), 5, False),
-        (Interval(max=0), 0, True),
-        (Interval(max=0), -1, True),
-        (Interval(min=0, max=1), 0, True),
-        (Interval(min=0, max=1), 1, True),
-        (Interval(min=0, max=1), 0.5, True),
-        (Interval(min=0, max=1), 2, False),
-        (Interval(min=0, max=1), -1, False),
+        (POSITIVE_OR_ZERO, 5, True),
+        (POSITIVE_OR_ZERO, 0, True),
+        (POSITIVE_OR_ZERO, -1, False),
+        (NEGATIVE_OR_ZERO, 5, False),
+        (NEGATIVE_OR_ZERO, 0, True),
+        (NEGATIVE_OR_ZERO, -1, True),
+        (ZERO_TO_ONE, 0, True),
+        (ZERO_TO_ONE, 1, True),
+        (ZERO_TO_ONE, 0.5, True),
+        (ZERO_TO_ONE, 2, False),
+        (ZERO_TO_ONE, -1, False),
     ],
 )
 def test_contains(interval, value, expected):
