dd269759bc12d8adcccbdcc3f87d7c114728dd81
aershov <codumentary.com@gmail.com>
2020-01-08 12:02:27 +0300

    remove bounds check from set_pixel
    Pillow does this check

--- src/raytracer/image.py
+++ src/raytracer/image.py
@@ -12,23 +12,7 @@ class Image:
         self._image.show()
 
     def set_pixel(self, x: int, y: int, color: Color) -> None:
-        self._check_inside(x, y)
         self._pixels[x, y] = (color.r, color.g, color.b)
 
     def get_pixel(self, x: int, y: int) -> Color:
-        # TODO: check that x,y in inside the image
         return Color(*self._pixels[x, y])
-
-    def _check_inside(self, x: int, y: int):
-        if not ((0 <= x < self._width) and (0 <= y < self._height)):
-            raise ValueError(
-                f"Point ({x}, {y}) is out of {self._width}x{self._height} image bounds"
-            )
-
-    @property
-    def _width(self) -> int:
-        return self._image.width
-
-    @property
-    def _height(self) -> int:
-        return self._image.height
--- src/tests/test_image.py
+++ src/tests/test_image.py
@@ -1,5 +1,3 @@
-import pytest
-
 from raytracer.color import Palette
 from raytracer.image import Image
 
@@ -8,9 +6,3 @@ def test_image():
     image = Image(300, 200)
     image.set_pixel(5, 10, Palette.BLACK)
     assert image.get_pixel(5, 10) == Palette.BLACK
-
-
-def test_set_pixel_out_of_bounds():
-    image = Image(300, 200)
-    with pytest.raises(ValueError):
-        image.set_pixel(300, 199, Palette.BLACK)
