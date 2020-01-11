fd1244f55d57cec2a5c881a5fba9ffe96e8dd0a2
aershov <codumentary.com@gmail.com>
2020-01-11 20:40:58 +0300

    add disabled regression test

--- src/tests/test_scene.py
+++ src/tests/test_scene.py
@@ -1,2 +1,19 @@
+import filecmp
+import os.path
+
+import pytest
+
+from raytracer import main
+
+
+@pytest.mark.skip(reason="slow")
 def test_scene():
-    assert True
+    main.render()._image.save(_full_path("test.png"))
+    assert filecmp.cmp(
+        _full_path("test.png"), _full_path("raytracer.png"), shallow=False
+    )
+    os.unlink(_full_path("test.png"))
+
+
+def _full_path(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
